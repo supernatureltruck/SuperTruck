955e687cb28d778aa55d5c25feefa04706022367
Chris Menn <cmennevret@gmail.com>
2019-02-01 16:20:23 +0100

    loginMAJ

--- src/app/_Comp/login/login.component.ts
+++ src/app/_Comp/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+  login(){
+    let link = ['menu'];
+    this.router.navigate(link);
+  }
+
+  annuler(){
+      history.back();
+  }
+
 
 }
